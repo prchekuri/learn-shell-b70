e82c0c9fdb5a8c139d8eb0ca50f3684249c09d27
Prameela <ch.prameelarani@gmail.com>
2023-01-12 17:59:06 +0530

    Shell Scripting Topics.

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 # If we assign a name to a set of data, then it is called as Variable.
 
-trainer=Raghu
-class=Devops
+trainer =Raghu
+class= Devops
 
 echo "The trainer is " $trainer
 echo "Trainer teaches " $class
