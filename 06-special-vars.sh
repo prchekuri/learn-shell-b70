79105de3148485e5c7475e4a594e7b29ca3d2bb0
Prameela <ch.prameelarani@gmail.com>
2023-01-16 15:16:26 +0530

    Special variables in shell script.

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -1,7 +1,20 @@
+echo "Following values are from Script"
 echo "0 - $0"
 echo "1 - $1"
 echo "2 - $2"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+
+example(){
+  echo "Following values are from example function"
+  echo "0 - $0"
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
+
+example
 
