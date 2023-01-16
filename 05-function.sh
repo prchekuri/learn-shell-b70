b9aa2b6abdcb318c0d596c875c28ae3f2a1ab3a3
Prameela <ch.prameelarani@gmail.com>
2023-01-16 13:28:40 +0530

    Functions.

---
+++ 05-function.sh
@@ -0,0 +1,11 @@
+#functions are like variables
+#Variable - When we assign a name to a set of data then it is a variable.
+#Function - When we assign a name to a set of commands then it is a function.
+
+#Function Declaration
+example(){
+  echo "I am a example function"
+}
+
+#call a function
+example
